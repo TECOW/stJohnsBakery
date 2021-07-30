1befbb890e6d63fb743f5ceacdf3c9c837b3b52e
Tecow Media <tecowmedia@gmail.com>
2021-07-29 20:30:50 -0400

    Fixed spacing

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -21,21 +21,21 @@ function Header() {
 
   return (
     <div className="headerDiv">
-      <h1 className="headerWebsiteName">Saint Johns Bakery</h1>
-    <Navbar fixed="top" expand="lg" className="homeNavBar">
-    <Container className="Headers">
-      <Row className="headerRow">
-        <Col className="navbarCol">
-          <h3 clssName="NavbarName" >Saint Johns Bakery</h3>
-        </Col>
-        <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Home" path="/"/></Col>
-        <Col className="headerButtons"><HomeButton className="homeButton" buttonName="About" path="/about"/></Col>
-        <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Menu" path="/menu"/></Col>
-        <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Order Online" path="/order"/></Col>
-        <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Contact" path="/contact"/></Col>
-      </Row>
-      </Container>
+      <Navbar fixed="top" expand="lg" className="homeNavBar">
+        <Container className="Headers">
+          <Row className="headerRow">
+            <Col className="navbarCol">
+              <h3 clssName="NavbarName" >Saint Johns Bakery</h3>
+            </Col>
+            <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Home" path="/"/></Col>
+            <Col className="headerButtons"><HomeButton className="homeButton" buttonName="About" path="/about"/></Col>
+            <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Menu" path="/menu"/></Col>
+            <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Order Online" path="/order"/></Col>
+            <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Contact" path="/contact"/></Col>
+          </Row>
+        </Container>
       </Navbar>
+      <h1 className="headerWebsiteName">Saint Johns Bakery</h1>
     </div>
   );
 
